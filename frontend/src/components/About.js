7772a843f9080000f29c1345043bc315e4158a6f
Nikkitpt <ndavis213@gmail.com>
2024-02-03 19:35:19 -0500

    updated CSS image text overlay

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,43 +1,90 @@
-import { Grid } from '@mui/material';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ImageWithText from './ImageWithText';
+
 
 export default function About() {
-    return(
-    <Grid container spacing={8} style={{ marginTop: '16px' }}>
-    {/* Items inside the Grid */}
-        <Grid item xs={6}>
-            <h2>What I Do </h2>
-            <p>Here's my two to three sentence thing about how im a badass</p>
-        </Grid>
-        <Grid item xs={6}>
-            <Accordion sx={{ backgroundColor: '#282c34', color: 'white'}} >
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    borderRadius: 20,
+    padding: 20,
+    backgroundColor: '#282c34',
+    overflow: 'hidden',
+  };
+
+  const headerStyle = {
+    color: '#f6e9d9',
+    marginTop: 0,
+  };
+
+  const contentContainerStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: '1',
+    width: '100%',
+  };
+
+  const contentStyle = {
+    flex: '1',
+    padding: '10px',
+    marginRight: '16px',
+    color: '#f6e9d9',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <div style={headerStyle}>
+        <h1>About Me</h1>
+        <ImageWithText/>
+      </div>
+      <div style={contentContainerStyle}>
+        <div style={contentStyle}>
+          <h2>What I Do</h2>
+          <p>Here's my two to three sentence thing about how I'm a badass</p>
+        </div>
+        <div style={{ flex: '1' }}>
+          <Accordion sx={{ backgroundColor: '#282c34', color: '#f6e9d9' }}>
             <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1-content"
-            id="panel1-header"
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1-content"
+              id="panel1-header"
             >
-            Custom Integrations
+              <h3>Custom Integrations</h3>
             </AccordionSummary>
-            <AccordionDetails style={{ color: 'white' }}>
-                Why im rad
+            <AccordionDetails style={{ color: '#f6e9d9' }}>
+              Why I'm rad
             </AccordionDetails>
-        </Accordion>
-        <Accordion sx={{ backgroundColor: '#282c34', color: 'white'}} >
+          </Accordion>
+          <Accordion sx={{ backgroundColor: '#282c34', color: '#f6e9d9' }}>
             <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel2-content"
-            id="panel2-header"
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1-content"
+              id="panel1-header"
             >
-            Something Else sick
+              <h3>Anotha one</h3>
             </AccordionSummary>
-        <AccordionDetails style={{ color: 'white' }}>
-            Why im rad
+            <AccordionDetails style={{ color: '#f6e9d9' }}>
+              Why I'm rad
             </AccordionDetails>
-        </Accordion>
-        </Grid>
-    </Grid>
-    );
+          </Accordion>
+          <Accordion sx={{ backgroundColor: '#282c34', color: '#f6e9d9' }}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              <h3>And Anotha one</h3>
+            </AccordionSummary>
+            <AccordionDetails style={{ color: '#f6e9d9' }}>
+              Why I'm rad
+            </AccordionDetails>
+          </Accordion>
+          {/* Additional accordions go here */}
+        </div>
+      </div>
+    </div>
+  );
 }

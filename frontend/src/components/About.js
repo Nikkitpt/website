6869f2966696f1bedc3eb53aae8803b754057da6
nikkitpt <ndavis213@gmail.com>
2024-02-08 20:37:02 -0500

    grid refactor for About + containers

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,36 +1,51 @@
 
 import {ImageWithText} from './ImageWithText';
-import Grid from '@mui/material/Unstable_Grid2'
-import { Container} from '@mui/material';
-
+// import Grid from '@mui/material/Unstable_Grid2'
+import { Grid } from '@mui/material'
 
 
 export function About() {
-  const containerStyle = {
-    // display: 'flex',
-    // flexDirection: 'column',
-    borderRadius: 20,
-    backgroundColor: '#282c34',
-    overflow: 'hidden',
-    paddingBottom: "0px",
-    paddingLeft: "200px",
-
-  };
 
   return (
-    <Container sx={containerStyle}>
-      <Grid container spacing={2}>
-        <ImageWithText/>
-        <Grid xs={6} sx={containerStyle}>
-          <h2>Integrations and Analytics</h2>
-          <p>I plan and implement custom third-party integrations to deliver comprehensive analytics and simplify report sharing.</p>
-          <h2>Data Migration</h2>
-          <p>I plan and implement custom third-party integrations to deliver comprehensive analytics and simplify report sharing.</p>
-          <h2>API Development</h2>
-          <p>Empowering all-scale startups with custom web experiences that are memorable and drive success. With a passion for design and development, I take projects from ideation to launch, ensuring a seamless journey that leaves a lasting impact in the digital landscape.</p>
+    <Grid sx={{
+      backgroundColor: '#282c34',
+      display: 'flex',
+      color: '#f6e9d9',
+      flexDirection: 'column',
+      borderRadius: 1,
+      alignItems: 'center',
+      px: 4,
+    }}>
+      <h1> About Me</h1>
+        {/* <ImageWithText/> */}
+
+    
+        <Grid container sx={{
+          display: 'flex',
+          flexFlow: 'column'
+        }}>
+          <Grid xs={12}>
+            <h2>Integrations and Analytics</h2>
+          </Grid>
+
+          <Grid md={6} xs={12}>
+            <p>I plan and implement custom third-party integrations to deliver comprehensive analytics and simplify report sharing.</p>
+          </Grid>
+          <Grid xs={12}>
+            <h2>Data Migration</h2>
+          </Grid>
+          <Grid md={6} xs={12}>
+            <p>I plan and implement custom third-party integrations to deliver comprehensive analytics and simplify report sharing.</p>
+          </Grid>
+          <Grid xs={12}>
+            <h2>API Development</h2>
+          </Grid>
+          <Grid md={6} xs={12}>
+            <p>Empowering all-scale startups with custom web experiences that are memorable and drive success. With a passion for design and development, I take projects from ideation to launch, ensuring a seamless journey that leaves a lasting impact in the digital landscape.</p>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+
+    </Grid>
 )
 }   
 
